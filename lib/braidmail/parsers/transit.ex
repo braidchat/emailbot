f2fcb4b2fa2015a81d86fb52c465e4f955c7c0ed
James N. V. Cash <james.nvc@gmail.com>
2017-05-29 21:04:53 -0400

    Parsing messages from transit+msgpack

--- lib/braidmail/parsers/transit.ex
+++ lib/braidmail/parsers/transit.ex
@@ -3,6 +3,9 @@ defmodule BraidMail.Parsers.Transit do
   Parsers msgpack-encoded Transit request body.
   """
 
+  def init(opts) do
+  end
+
   @behaviour Plug.Parsers
   import Plug.Conn
 
@@ -34,8 +37,30 @@ defmodule BraidMail.Parsers.Transit do
 
   defp decode({:ok, body, conn}) do
     case MessagePack.unpack(body) do
-      {:ok, parsed} -> {:ok, parsed, conn}
+      {:ok, parsed} -> {:ok, from_transit(parsed), conn}
       _ -> raise Plug.Parsers.ParseError
     end
+  end
+
+  defp from_transit(map) do
+    Enum.into map, %{}, fn {k, v} ->
+      {parse_transit_item(k), parse_transit_item(v)}
+    end
+  end
+
+  defp parse_transit_item("~:" <> kw) do
+    String.to_atom(kw)
+  end
+
+  defp parse_transit_item(["~#u", [hi64, lo64]]) do
+    UUID.binary_to_string!(<<hi64::64>> <> <<lo64::64>>)
+  end
+
+  defp parse_transit_item(arr) when is_list(arr) do
+    Enum.map arr, &parse_transit_item/1
+  end
+
+  defp parse_transit_item(x) do
+    x
   end
 end
