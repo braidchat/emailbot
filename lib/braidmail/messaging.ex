db5a9095af0d24cd06ef34c1daa23bc8e767d1f7
James N. V. Cash <james.nvc@gmail.com>
2017-06-01 17:38:41 -0400

    Show help msg on first msg, greeting on subsequent

--- lib/braidmail/messaging.ex
+++ lib/braidmail/messaging.ex
@@ -4,8 +4,10 @@ defmodule BraidMail.Messaging do
   """
 
   alias BraidMail.Braid
+  alias BraidMail.Repo
+  alias BraidMail.Schemas.User
 
-  def handle_message(%{content: body} = msg) do
+  def handle_message(%{content: body, "thread-id": thread_id} = msg) do
     bot_name = Application.fetch_env!(:braidmail, :bot_name)
     prefix = "/" <> bot_name <> " "
     if String.starts_with?(body, prefix) do
@@ -15,8 +17,29 @@ defmodule BraidMail.Messaging do
     end
   end
 
-  defp handle_mention(msg) do
+  @usage_msg """
+  Hi, I'm the email bot!
+  I can help you manage your gmail inbox from braid
+  """
+
+  defp handle_mention(%{"user-id": user_id} = msg) do
     IO.puts "Mentioned: #{inspect msg}"
-    msg |> Braid.make_response("Hello there!") |> Braid.send_message
+    if Repo.get_by(User, braid_id: user_id) do
+      msg
+        |> Braid.make_response("Hi again! " <> uuid2mention(user_id))
+        |> add_mentioned(user_id)
+        |> Braid.send_message
+    else
+      Repo.insert(%User{braid_id: user_id})
+      msg |> Braid.make_response(@usage_msg) |> Braid.send_message
+    end
+  end
+
+  defp uuid2mention("urn:uuid:" <> uuid) do
+    "@" <> uuid
+  end
+
+  defp add_mentioned(%{"mentioned-user-ids": mentions} = msg, user_id) do
+    %{msg | "mentioned-user-ids": [user_id | mentions]}
   end
 end
