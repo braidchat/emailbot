d70b69230d464e2ad3f0d50765298149ffac4a8a
James N. V. Cash <james.nvc@gmail.com>
2017-06-01 18:09:47 -0400

    Stubbing out thread messages

--- lib/braidmail/messaging.ex
+++ lib/braidmail/messaging.ex
@@ -6,14 +6,17 @@ defmodule BraidMail.Messaging do
   alias BraidMail.Braid
   alias BraidMail.Repo
   alias BraidMail.Schemas.User
+  alias BraidMail.Schemas.Thread
 
   def handle_message(%{content: body, "thread-id": thread_id} = msg) do
     bot_name = Application.fetch_env!(:braidmail, :bot_name)
     prefix = "/" <> bot_name <> " "
-    if String.starts_with?(body, prefix) do
-      handle_mention(msg)
-    else
-      IO.puts "Got message #{body}"
+    cond do
+      String.starts_with?(body, prefix) ->
+        handle_mention(msg)
+      Repo.get_by(Thread, braid_id: thread_id) ->
+        handle_email_msg(msg)
+      true -> IO.puts "Got message #{body}"
     end
   end
 
@@ -33,6 +36,9 @@ defmodule BraidMail.Messaging do
       Repo.insert(%User{braid_id: user_id})
       msg |> Braid.make_response(@usage_msg) |> Braid.send_message
     end
+  end
+
+  defp handle_email_msg(%{content: body, "thread-id": thread_id} = msg) do
   end
 
   defp uuid2mention("urn:uuid:" <> uuid) do
