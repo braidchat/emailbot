1a5acdba8b0076daa304b5a5359358450cd6faa3
James N. V. Cash <james.nvc@gmail.com>
2017-06-06 17:43:55 -0400

    Refactoring messages vis-à-vis user state

--- lib/braidmail/messaging.ex
+++ lib/braidmail/messaging.ex
@@ -9,23 +9,7 @@ defmodule BraidMail.Messaging do
   alias BraidMail.Schemas.Thread
   alias BraidMail.Schemas.User
 
-  def handle_message(%{content: body, "thread-id": thread_id} = msg) do
-    bot_name = Application.fetch_env!(:braidmail, :bot_name)
-    prefix = "/" <> bot_name <> " "
-    cond do
-      String.starts_with?(body, prefix) ->
-        %{msg | content: String.replace_prefix(body, prefix, "")}
-          |> handle_mention()
-      Repo.get_by(Thread, braid_id: thread_id) -> handle_email_msg(msg)
-      true -> IO.puts "Got message #{body}"
-    end
-  end
-
-  @usage_msg """
-  Hi, I'm the email bot!
-  I can help you manage your gmail inbox from braid
-  """
-
+  # Various message strings that will be sent
   @gmail_signup_msg """
   Hi ~s! Looks like you haven't connected your gmail account yet.
   Say `/~s connect` and I'll send you a link to get started.
@@ -42,39 +26,16 @@ defmodule BraidMail.Messaging do
   Invoke me with `/~s help` for a list of things you can do now :)
   """
 
-  defp handle_mention(%{"user-id": user_id, content: "connect"}) do
-    Braid.send_message(%{id: UUID.uuid4(:urn),
-                         "thread-id": UUID.uuid4(:urn),
-                         content: @connect_link_msg
-                                  |> :io_lib.format([Gmail.oauth_uri(user_id)])
-                                  |> to_string,
-                         "mentioned-user-ids": [user_id],
-                         "mentioned-tag-ids": []})
-  end
+  @connected_help_msg """
+  Cool, you're connected! I know the following commands:
+  `inbox` - I'll tell you the subject & senders of your email inbox
+  `compose` - I'll start a new thread you can use to compose an email
+  `read` - ???
+  """
 
-  defp handle_mention(%{"user-id": user_id} = msg) do
-    bot_name = Application.fetch_env!(:braidmail, :bot_name)
-    case Repo.get_by(User, braid_id: user_id) do
-      %User{braid_id: user_id, gmail_token: tok} when tok != nil ->
-        msg
-          |> Braid.make_response("Hi again! " <> uuid2mention(user_id))
-          |> add_mentioned(user_id)
-          |> Braid.send_message
-      %User{braid_id: user_id} ->
-        msg
-          |> Braid.make_response(
-              @gmail_signup_msg
-                |> :io_lib.format([uuid2mention(user_id), bot_name])
-                |> to_string
-              )
-          |> add_mentioned(user_id)
-          |> Braid.send_message
-      _ ->
-        Repo.insert(%User{braid_id: user_id})
-        msg |> Braid.make_response(@usage_msg) |> Braid.send_message
-    end
-  end
-
+  @doc """
+  Send a message to the given user after their gmail account has been connected
+  """
   def send_connected_msg(user_id) do
     bot_name = Application.fetch_env!(:braidmail, :bot_name)
     Braid.send_message(%{id: UUID.uuid4(:urn),
@@ -86,6 +47,72 @@ defmodule BraidMail.Messaging do
                          "mentioned-tag-ids": []})
   end
 
+  @doc """
+  Entry point for receiving messages from Braid
+  """
+  def handle_message(%{"user-id": user_id, content: body,
+                       "thread-id": thread_id} = msg) do
+    bot_name = Application.fetch_env!(:braidmail, :bot_name)
+    prefix = "/" <> bot_name <> " "
+    case Repo.get_by(User, braid_id: user_id) do
+      %User{braid_id: user_id, gmail_token: tok} when tok != nil ->
+        cond do
+          String.starts_with?(body, prefix) ->
+            msg
+            |> remove_msg_prefix(prefix)
+            |> handle_mention()
+          Repo.get_by(Thread, braid_id: thread_id) -> handle_email_msg(msg)
+          true -> IO.puts "Got message #{body}"
+        end
+      %User{braid_id: user_id} ->
+        msg
+        |> remove_msg_prefix(prefix)
+        |> send_initial_msg()
+      _ ->
+        Repo.insert(%User{braid_id: user_id})
+        msg
+        |> remove_msg_prefix(prefix)
+        |> send_initial_msg()
+    end
+  end
+
+  # Handle mentions from a user who is not yet connected to gmail
+  defp send_initial_msg(%{"user-id": user_id, content: "connect"}) do
+    Braid.send_message(%{id: UUID.uuid4(:urn),
+                         "thread-id": UUID.uuid4(:urn),
+                         content: @connect_link_msg
+                                  |> :io_lib.format([Gmail.oauth_uri(user_id)])
+                                  |> to_string,
+                         "mentioned-user-ids": [user_id],
+                         "mentioned-tag-ids": []})
+  end
+
+  defp send_initial_msg(%{"user-id": user_id} = msg) do
+    bot_name = Application.fetch_env!(:braidmail, :bot_name)
+    msg
+    |> Braid.make_response(
+        @gmail_signup_msg
+        |> :io_lib.format([uuid2mention(user_id), bot_name])
+        |> to_string)
+    |> add_mentioned(user_id)
+    |> Braid.send_message
+  end
+
+  # Handling mentions from already connected users
+  defp handle_mention(%{"user-id": user_id, content: "help"} = msg) do
+    msg
+    |> Braid.make_response(@connected_help_msg)
+    |> Braid.send_message
+  end
+
+  defp handle_mention(%{"user-id": user_id} = msg) do
+    # TODO: send some sort of status info to already connected user
+    msg
+    |> Braid.make_response("Hi again! " <> uuid2mention(user_id))
+    |> add_mentioned(user_id)
+    |> Braid.send_message
+  end
+
   defp handle_email_msg(%{content: _body, "thread-id": _thread_id} = _msg) do
   end
 
@@ -95,5 +122,9 @@ defmodule BraidMail.Messaging do
 
   defp add_mentioned(%{"mentioned-user-ids": mentions} = msg, user_id) do
     %{msg | "mentioned-user-ids": [user_id | mentions]}
+  end
+
+  defp remove_msg_prefix(%{content: content} = msg, prefix) do
+    %{msg | content: String.replace_prefix(content, prefix, "")}
   end
 end
